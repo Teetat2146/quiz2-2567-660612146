85c60cb3bdcde2ad0a524fc6e8550b18905f0b33
Teetat Yodbun <teetatjj1@gmail.com>
2024-08-08 14:35:27 +0700

    s

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,15 +1,19 @@
 "use client";
+import { CommentProps } from "@/libs/types";
+
+
 export default function Comment({
   ImagePath,
   username,
   commentTitle,
   likes,
   replies,
-}) {
+}):CommentProps {
   return (
     <div>
       your code for Comment component here ...
       {/* You can use map-loop to render Reply component here */}
+      const comment = 
     </div>
   );
 }
